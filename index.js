a6e59d15de45fa4af4b0b79dfb5920bff7dd1ea5
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-13 15:33:50 -0700

    created first reducer

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// action
-const BUY_CAKE = 'BUY_CAKE'
-{
-    type:BUY_CAKE,
-    info:'First Redux Action'
-}
 //action creator
 function buyCake(){
     return{
         type:BUY_CAKE,
         info:'First redux action'
+    }
+}
+
+//reducer | (previousState,action)=>newState
+const initialState = {
+    numOfCakes:10
+}
+const reducer = (state=initialState,action) =>{
+    switch(action.type){
+        case BUY_CAKE:return{
+            numOfCakes:state.numOfCakes-1
+        }
+        default:return state
     }
 }
