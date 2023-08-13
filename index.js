5df749dd3ec03a9a4e4957d1a2d254cde97e292e
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-13 16:05:30 -0700

    created a store

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
 //action creator
 function buyCake(){
     return{
@@ -13,8 +18,20 @@ const initialState = {
 const reducer = (state=initialState,action) =>{
     switch(action.type){
         case BUY_CAKE:return{
+            ...state,
             numOfCakes:state.numOfCakes-1
         }
         default:return state
     }
 }
+
+// Creating store
+const store = createStore(reducer)
+console.log('Initial State : ',store.getState())
+const unsubscribe = store.subscribe(()=>console.log('Updated State : ' , store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
+
+
