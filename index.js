bdad2c70b024f83dc96bca2f95637e4b3a159b83
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-13 15:17:55 -0700

    created first action

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('From index.js')
+// action
+const BUY_CAKE = 'BUY_CAKE'
+{
+    type:BUY_CAKE,
+    info:'First Redux Action'
+}
+//action creator
+function buyCake(){
+    return{
+        type:BUY_CAKE,
+        info:'First redux action'
+    }
+}
