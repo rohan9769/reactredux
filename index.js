d784edfafb51c5b38e362dacf9c0d1fbe02847f3
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-13 16:52:17 -0700

    combined reducers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
 
@@ -21,10 +22,22 @@ const buyTea = () =>{
 }
 
 //reducer | (previousState,action)=>newState
+/*
 const initialState = {
     numOfCakes:10,
     numOfTeas:15
 }
+*/
+
+const initialCakeState = {
+    numOfCakes:10
+}
+const initialTeaState = {
+    numOfTeas:20
+}
+
+// Single Reducer
+/*
 const reducer = (state=initialState,action) =>{
     switch(action.type){
         case BUY_CAKE:return{
@@ -38,9 +51,41 @@ const reducer = (state=initialState,action) =>{
         default:return state
     }
 }
+*/
 
+// Creating multiple reducer
+const cakeReducer = (state = initialCakeState , action) =>{
+    switch(action.type){
+        case BUY_CAKE:
+            return{
+                ...state,
+                numOfCakes:state.numOfCakes-1
+            }
+        default:
+            return state
+    }
+}
+
+const teaReducer = (state=initialTeaState,action)=>{
+    switch(action.type){
+        case BUY_TEA:
+            return{
+                ...state,
+                numOfTeas:state.numOfTeas-1
+            }
+        default:
+            return state
+    }
+}
+
+// Combining Reducers
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    tea:teaReducer
+})
 // Creating store
-const store = createStore(reducer)
+// const store = createStore(reducer)
+const store = createStore(rootReducer)
 console.log('Initial State : ',store.getState())
 const unsubscribe = store.subscribe(()=>console.log('Updated State : ' , store.getState()))
 store.dispatch(buyCake())
